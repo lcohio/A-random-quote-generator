1457f274d53426687e43c0774d278874ff684ec8
lcohio <coninglogan@gmail.com>
2019-07-17 21:54:18 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const randomQuotes = [
+    {
+        quote: '"Dream big and dare to fail"',
+		source: "Norman Vaughan",
+		citation: "",
+		year: "1905 - 2005",
+    },
+    {
+        quote: '"What we think, we become"',
+		source: "Buddha",
+		citation: "",
+		year: "",
+    },
+    {
+        quote: '"It always seems impossible until it is done."',
+		source: "Nelson Mandela",
+		citation: "",
+		year: "1918 - 2013",
+    },
+    {
+        quote: '"The only thing to fear is fear itself."',
+	    source: 'Franklin Delano Roosevelt',
+	    citation: 'First Inaugural Address',
+	    year: "1932",
+    },
+    {
+        quote: '"That\'s one small step for man, one giant leap for mankind."',
+		source: "Neil Armstrong",
+		citation: "The moon",
+		year: "1969",
+    },
+    {
+        quote: '"Not everything that can be counted counts, and not everything that counts can be counted."',
+		source: "Albert Einstein",
+		citation: "",
+		year: "1879 - 1955",
+    },
+    {
+        quote: '"Not everything that can be counted counts, and not everything that counts can be counted."',
+		source: "Albert Einstein",
+		citation: "",
+		year: "1879 - 1955",
+    }
+]
